afcaf2a4d718edf1184044510085b74cdbe8d3c5
Suman Neupane <sumanmyon.sam@gmail.com>
2019-04-26 12:10:00 +0545

    Banner Created and loaded

--- app/src/main/java/www/sumanmyon/com/admobbeginner/MainActivity.java
+++ app/src/main/java/www/sumanmyon/com/admobbeginner/MainActivity.java
@@ -3,9 +3,16 @@ package www.sumanmyon.com.admobbeginner;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    private AdView adView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +21,14 @@ public class MainActivity extends AppCompatActivity {
 
         //initalize AdMob
         MobileAds.initialize(this,"ca-app-pub-3940256099942544~3347511713");
+
+        //initalizing adView
+        adView = findViewById(R.id.adView);
+
+        //requesting ad
+        AdRequest adRequest = new AdRequest.Builder().build();
+
+        //loading ad
+        adView.loadAd(adRequest);
     }
 }
