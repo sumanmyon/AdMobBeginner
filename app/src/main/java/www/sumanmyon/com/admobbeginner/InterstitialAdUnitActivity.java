7c5b26ef26ba3d5c96bab281b835a3f0b60a4dc8
Suman Neupane <sumanmyon.sam@gmail.com>
2019-04-26 12:34:02 +0545

    InterstitialAd created

---
+++ app/src/main/java/www/sumanmyon/com/admobbeginner/InterstitialAdUnitActivity.java
@@ -0,0 +1,14 @@
+package www.sumanmyon.com.admobbeginner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InterstitialAdUnitActivity extends AppCompatActivity {
+
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interstitial_ad_unit);
+    }
+}
