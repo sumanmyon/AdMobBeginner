ffa05a68855ce690a8832c9a55c386cbfda3b1fb
Suman Neupane <sumanmyon.sam@gmail.com>
2019-04-26 11:23:00 +0545

    AdMob Integrated

--- app/src/main/java/www/sumanmyon/com/admobbeginner/MainActivity.java
+++ app/src/main/java/www/sumanmyon/com/admobbeginner/MainActivity.java
@@ -3,11 +3,16 @@ package www.sumanmyon.com.admobbeginner;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.ads.MobileAds;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initalize AdMob
+        MobileAds.initialize(this,"ca-app-pub-3940256099942544~3347511713");
     }
 }
